339d5ed652bb5dc55475b57a08569b2f10dbe95f
Cloudo <cloudo.shifa@gmail.com>
2019-04-17 19:48:06 +0300

    added lib directory to skip build process

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+export * from 'serialize-query-params';
+export * from './types';
+export { useQueryParam } from './useQueryParam';
+export { useQueryParams } from './useQueryParams';
+export { updateUrlQuery } from './updateUrlQuery';
+export { QueryParamProvider, QueryParamContext, QueryParamContextValue, } from './QueryParamProvider';
